37dd3fa1bfb2603b26732289ff5cc3ee1a4436ae
sgcm14 <sgcm14@gmail.com>
2025-06-20 16:55:02 -0500

    finish hooks

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
 
-  const incrementFather = () => {
-    setCounter(counter + 1);
-  };
+  const incrementFather = useCallback((value) => {
+    setCounter((c) => c + value);
+  }, []);
+
+  useEffect(() => {
+    // incrementFather();
+  }, [incrementFather]);
+
+  // const incrementFather = () => {
+  //   setCounter(counter + 1);
+  // };
+
   return (
     <>
       <h1>useCallback Hook:{counter}</h1>
--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -1,12 +1,16 @@
-export const ShowIncrement = ({ increment }) => {
+import React from "react";
+
+export const ShowIncrement = React.memo(({ increment }) => {
+  console.log("Me volví a generar :(");
+
   return (
     <button
       className="btn btn-primary"
       onClick={() => {
-        increment();
+        increment(5);
       }}
     >
       Incrementar
     </button>
   );
-};
+});
